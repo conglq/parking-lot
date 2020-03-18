e3034b5efbde09dabac8238b915459f12b6aa914
Le Quang Cong <leqcong@gmail.com>
2020-03-18 15:47:16 +0700

    setup project

---
+++ bin/run_functional_tests/read-input.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Read Input File', function () {
+  it('is existed file in bin/parking_lot/file_inputs.txt', function (done) {
+    done();
+  });
+});
